2581e28895263a6473a0cfe3e6b078894c13658a
lizzaran <lizzaran92@gmail.com>
2015-10-05 02:31:45 +0200

    Update

--- SFXChallenger/Properties/AssemblyInfo.cs
+++ SFXChallenger/Properties/AssemblyInfo.cs
@@ -61,6 +61,6 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("0.9.0.14")]
-[assembly: AssemblyFileVersion("0.9.0.14")]
-[assembly: AssemblyInformationalVersion("0.9.0.14")]
+[assembly: AssemblyVersion("0.9.0.15")]
+[assembly: AssemblyFileVersion("0.9.0.15")]
+[assembly: AssemblyInformationalVersion("0.9.0.15")]
